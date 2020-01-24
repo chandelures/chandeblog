dcb603668d383a14bbcd8656208a71034de31782
chandelurewang@qq.com <chandelurewang@qq.com>
2020-01-25 02:51:56 +0800

    update: ui-blog

--- static/blog/js/base.js
+++ static/blog/js/base.js
@@ -1,5 +1,181 @@
 (function () {
     "use strict";
+
+    window.blog = {
+        handler: {},
+    };
+
+    blog.handler = {
+        //回到顶部功能
+        toUp: function ($button) {
+            var fadeInTime = 300,
+                fadeOutTime = 300,
+                scrollSpace = 300,
+                animateTime = 500;
+            $button.click(function () {
+                $('html,body').animate({scrollTop: 0}, animateTime);
+            });
+            $(window)
+                .scroll(function () {
+                    if ($(this).scrollTop() > scrollSpace) {
+                        $button.fadeIn(fadeInTime);
+                    } else {
+                        $button.fadeOut(fadeOutTime);
+                    }
+                })
+                .scroll()
+            ;
+
+        },
+
+        initHighlighting: function(){
+          hljs.initHighlightingOnLoad();
+        },
+
+        //将指定标签变为激活状态
+        activeAnchor: function ($header) {
+            var id = $header.attr("id");
+            $("[href='#" + id + "']").addClass("active");
+        },
+
+        //将指定标签变为非激活状态
+        deactiveAnchor: function ($toc, level) {
+            if (level === 1) {
+                $toc.find("ul > li > a").removeClass("active");
+            } else if (level === 2) {
+                $toc.find("ul > li > ul > li > a").removeClass("active");
+            }
+        },
+
+        //判断页面是否到达底部
+        isArriveBottom: function () {
+            var $window = $(window),
+                $document = $(document),
+                error = 2,
+                condition = $window.scrollTop() + $window.height() - $document.height();
+            return (condition < error && condition > -error);
+        },
+
+        //初始化目录
+        initToc: function ($toc, $content) {
+            var
+                $h = $content.find("h2,h3"),
+                $window = $(window),
+                offset = 60;
+            $toc
+                .find("ul")
+                .addClass("ui link list")
+            ;
+            $toc
+                .find("a")
+                .addClass("item")
+            ;
+            $toc
+                .find("a")
+                .on("click", function (e) {
+                    e = e || window.event;
+                    e.preventDefault();
+                    var target = $(this).prop('hash');
+                    $('html,body').scrollTop($(target).offset().top - offset + 2);
+                })
+            ;
+            $window
+                .scroll(function () {
+                    $h.each(function () {
+                            var $this = $(this);
+                            if (this.tagName === "H2") {
+                                if ($window.scrollTop() > $this.offset().top - offset) {
+                                    blog.handler.deactiveAnchor($toc, 1);
+                                    blog.handler.activeAnchor($this);
+                                }
+                            } else if (this.tagName === "H3") {
+                                if ($window.scrollTop() > $this.offset().top - offset) {
+                                    blog.handler.deactiveAnchor($toc, 2);
+                                    blog.handler.activeAnchor($this);
+                                }
+                            }
+                        }
+                    );
+                    if (blog.handler.isArriveBottom()) {
+                        var last = $h[$h.length - 1];
+                        if (last.tagName === "H2") {
+                            blog.handler.deactiveAnchor($toc, 1);
+                            blog.handler.activeAnchor($(last));
+                        } else if (last.tagName === "H3") {
+                            blog.handler.deactiveAnchor($toc, 2);
+                            blog.handler.activeAnchor($(last));
+                        }
+                    }
+                })
+            ;
+        },
+
+        //扩展文章列表
+        appendPostList: function (item) {
+            var
+                post_url = item.post_url,
+                post_title = item.post_title,
+                post_category_name = item.post_category_name,
+                post_abstract = item.post_abstract,
+                post_create_date = item.post_create_date,
+                post_views = item.post_views,
+                postListContainer = $("#postList .ui.stackable.grid"),
+                postListDom = "<div class=\"ui row\">\n" +
+                    "                <div class=\"four wide column\">\n" +
+                    "                    <time>" + post_create_date + "</time>\n" +
+                    "                    <p class=\"category\">" + post_category_name + "</p>\n" +
+                    "                </div>\n" +
+                    "                <div class=\"eleven wide column\">\n" +
+                    "                    <h3><a href=\"" + post_url + "\">" + post_title + "</a></h3>\n" +
+                    "                    <div>" + post_abstract + "</div>\n" +
+                    "                    <h4 class=\"ui horizontal header divider\">\n" +
+                    "                        <a href=\"" + post_url + "\">Read More »</a>\n" +
+                    "                    </h4>\n" +
+                    "                </div>\n" +
+                    "            </div>";
+            postListContainer
+                .append(postListDom)
+            ;
+        },
+
+        //加载文章列表
+        loadPostList: function ($button, loadPostCount = 5) {
+            var index = 0,
+                postCount = 0,
+                apiURL = "/api/getpostlist";
+            $button
+                .click(function () {
+                    $.getJSON({
+                        url: apiURL,
+                        type: 'get',
+                        data: {
+                            'index': index,
+                            'count': loadPostCount
+                        },
+                        success: function (data) {
+                            postCount = data.post_count;
+                            var postList = data.post_list;
+                            $.each(postList, function (i, item) {
+                                if (index < postCount) {
+                                    index += 1;
+                                    blog.handler.appendPostList(item);
+                                    $button
+                                        .removeClass('hidden')
+                                    ;
+                                }
+                                if (postList.length === 0 || index === postCount)
+                                    $button
+                                        .addClass('hidden')
+                                    ;
+                            });
+                        }
+                    });
+                })
+                .click()
+            ;
+        }
+    };
+
     var ready = function () {
         var
             $sidebar = $(".ui.sidebar"),
@@ -8,7 +184,7 @@
 
         //sidebar的展开和折叠
         $sidebar
-            .sidebar('attach events', '.toc.item')
+            .sidebar('attach events', '.expand.item')
         ;
 
         //顶部导航栏
---
+++ static/blog/js/index.js
@@ -0,0 +1,19 @@
+(function () {
+    "use strict";
+
+    var ready = function () {
+        var
+            $toUpButton = $("#toUp"),
+            $loadPostListButton = $("#loadPostListButton");
+
+        blog.handler.loadPostList($loadPostListButton, 5);
+
+        blog.handler.toUp($toUpButton);
+
+        blog.handler.initHighlighting();
+    };
+
+    $(function () {
+        ready();
+    });
+})();
