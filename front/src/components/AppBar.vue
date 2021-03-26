51feb9ed0c2eccf88ebe8bee40c36a6a364f7c80
chandelures <chandelurewang@gmail.com>
2021-03-26 22:52:21 +0800

    add: icon and logo

--- front/src/components/AppBar.vue
+++ front/src/components/AppBar.vue
@@ -2,8 +2,18 @@
   <v-app-bar app color="green darken-3" dark>
     <v-container class="py-1 fill-height ">
       <v-row>
-        <v-col lg="8" offset-lg="2" md="10" offset-md="1" cols="12">
-          <v-avatar color="white" class="mr-8" size="35"> </v-avatar>
+        <v-col
+          lg="8"
+          offset-lg="2"
+          md="10"
+          offset-md="1"
+          cols="12"
+          class="d-flex"
+        >
+          <router-link class="my-auto mr-6" to="/">
+            <v-img alt="CBLOG" src="../assets/img/logo.png" width="100" contain>
+            </v-img>
+          </router-link>
           <v-btn to="/" text plain> Home </v-btn>
           <v-btn to="/about" text plain> About </v-btn>
           <v-spacer> </v-spacer>
