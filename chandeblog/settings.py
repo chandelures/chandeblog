0d277f9c9095980ff28a88fe4d6443ec474a6ea4
chandelures <chandelurewang@gmail.com>
2021-03-30 20:58:35 +0800

    update: send error email in prod env

--- chandeblog/settings.py
+++ chandeblog/settings.py
@@ -188,17 +188,27 @@ if config.ENV == 'prod':
         },
         'handlers': {
             'file': {
-                'level': 'WARNING',
+                'level': DJANGO_LOG_LEVEL,
                 'class': 'logging.FileHandler',
                 'formatter': 'default',
                 'filename': Path(LOGS_DIR).joinpath('django.log'),
             },
+            'mail_admins': {
+                'level': 'ERROR',
+                'class': 'django.utils.log.AdminEmailHandler',
+                'include_html': True,
+            }
         },
         'loggers': {
             'django': {
                 'handlers': ['file'],
                 'level': DJANGO_LOG_LEVEL,
                 'propagate': True,
+            },
+            'django.request': {
+                'handlers': ['mail_admins'],
+                'level': 'ERROR',
+                'propagate': False,
             },
         },
     }
