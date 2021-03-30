f3e6ebe382f71f1aa63c592e245c88c6e08cbc49
chandelurewang <chandelurewang@gmail.com>
2021-03-30 13:56:12 +0800

    fix: delete the last of slah in the api path

--- blog/urls.py
+++ blog/urls.py
@@ -5,21 +5,21 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('articles/', views.ArticleList.as_view(), name='article-list'),
+    path('articles', views.ArticleList.as_view(), name='article-list'),
 
-    re_path(r'^articles/(?P<slug>[-\w]+)/$',
+    re_path(r'^articles/(?P<slug>[-\w]+)$',
             views.ArticleDetail.as_view(), name='article-detail'),
 
-    path('create/articles/',
+    path('create/articles',
          views.ArticleCreate.as_view(), name='article-create'),
 
-    path('categories/', views.CategoryList.as_view(), name='category-list'),
+    path('categories', views.CategoryList.as_view(), name='category-list'),
 
-    re_path(r'^categories/(?P<slug>[-\w]+)/$',
+    re_path(r'^categories/(?P<slug>[-\w]+)$',
             views.CategoryDetail.as_view(), name='category-detail'),
 
-    path('create/categories/', views.CategoryCreate.as_view(),
+    path('create/categories', views.CategoryCreate.as_view(),
          name='category-create'),
 
-    path('about/', views.AboutDetail.as_view(), name='about'),
+    path('about', views.AboutDetail.as_view(), name='about'),
 ]
