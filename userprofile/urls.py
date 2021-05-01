e186670d036d8e6039570f3a28b6b6b3b1d7645b
chandelures <chandelurewang@gmail.com>
2021-05-02 01:18:17 +0800

    fix: update auth api style

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 from userprofile import views
 
 app_name = 'userprofile'
 
 urlpatterns = [
-    path('profile', views.UserProfileDetail.as_view(), name="profile"),
-    path('', views.UserProfileList.as_view(), name='list'),
+    path('users/profile', views.UserProfileDetail.as_view(), name="profile"),
+    path('users', views.UserProfileList.as_view(), name='list'),
+
+    path("token/login", obtain_auth_token, name='login'),
+    path('token/logout', views.Logout.as_view(), name='logout'),
 ]
