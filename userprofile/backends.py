3f8438e893d2e7783bf845039e167dd64e275042
chandelures <chandelurewang@gmail.com>
2021-05-02 16:39:55 +0800

    fix: drop unuse statement in custom auth backend

--- userprofile/backends.py
+++ userprofile/backends.py
@@ -6,10 +6,6 @@ User = get_user_model()
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        if username is None:
-            username = kwargs.get(User.USERNAME_FIELD)
-        if username is None or password is None:
-            return
         try:
             user = User._default_manager.get(email=username)
         except User.DoesNotExist:
