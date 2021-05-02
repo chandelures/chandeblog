b11c2c06083272f749cbfc1e1f07109fe58d2292
chandelures <chandelurewang@gmail.com>
2021-05-03 00:59:22 +0800

    refactor: fix fake script main entry is too complex

--- scripts/fake.py
+++ scripts/fake.py
@@ -1,87 +1,87 @@
 import os
 import sys
-from datetime import timedelta
 from pathlib import Path
 
 import django
 import faker
-from django.utils import timezone
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(BASE_DIR)
 
 
-if __name__ == '__main__':
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chandeblog.settings")
-    django.setup()
+fake = faker.Faker('zh_CN')
 
-    from django.contrib.auth import get_user_model
-    from blog.models import Category, Article
-    from comment.models import Comment
 
-    User = get_user_model()
-
+def clean_database():
     print('clean database')
     Article.objects.all().delete()
     Category.objects.all().delete()
     User.objects.all().delete()
+    Comment.objects.all().delete()
 
+
+def create_superuser():
     print('create super user')
+    global user
     user = User.objects.create_superuser('admin', 'admin@example.com', 'admin')
-    categories = ['Python学习笔记', '开源项目', '工具资源', '程序员生活感悟', 'test category']
-    a_year_ago = timezone.now() - timedelta(days=365)
 
+
+def create_categories():
     print('create categories')
+    categories = ['Python学习笔记', '开源项目', '工具资源', '程序员生活感悟', 'test category']
     for category in categories:
         Category.objects.create(name=category)
 
+
+def create_articles():
     print('create some articles by using Faker')
-    fake = faker.Faker('zh_CN')
+
+    def section():
+        return '## ' + fake.sentence().rstrip('.') + '\n\n' + \
+            fake.paragraph(10) + '\n\n' + fake.paragraph(10) + '\n\n'
+
+    def subsection():
+        return '### ' + fake.sentence().rstrip('.') + '\n\n' + \
+            fake.paragraph(10) + '\n\n' + fake.paragraph(10) + \
+            '\n\n' + fake.paragraph(10) + '\n\n'
+
+    def subsections(count):
+        results = ''
+        for j in range(count):
+            results = results + subsection()
+        return results
+
     for i in range(100):
         category = Category.objects.order_by('?').first()
-        created = fake.date_time_between(
-            start_date='-1y', end_date='now', tzinfo=timezone.get_current_timezone())
         title = fake.sentence().rstrip('.')
         abstract = fake.paragraph(8) + '\n\n' + fake.paragraph(8) + '\n\n'
-
-        def section():
-            return '## ' + fake.sentence().rstrip('.') + '\n\n' + \
-                fake.paragraph(10) + '\n\n' + fake.paragraph(10) + '\n\n'
-
-        def subsection():
-            return '### ' + fake.sentence().rstrip('.') + '\n\n' + \
-                fake.paragraph(10) + '\n\n' + fake.paragraph(10) + \
-                '\n\n' + fake.paragraph(10) + '\n\n'
-
-        def subsections(count):
-            results = ''
-            for j in range(count):
-                results = results + subsection()
-            return results
 
         content = abstract + section() + section() + subsections(2) + section() + \
             subsections(3)
 
         Article.objects.create(
             title=title,
-            category=Category.objects.order_by('?').first(),
+            category=category,
             abstract=abstract,
             content=content,
-            created=created,
             author=user,
         )
 
+
+def create_md_sample_article():
     print('create a sample article')
     Article.objects.create(
         title='博客文章 Markdown 测试',
-        abstract=abstract,
+        abstract=fake.paragraph(),
         content=Path(BASE_DIR).joinpath(
             'scripts', 'example.md').read_text(encoding='utf-8'),
         category=Category.objects.create(name='Markdown测试'),
         author=user,
     )
 
+
+def create_users():
     print('create some users')
     for i in range(10):
         User.objects.create_user(
@@ -89,6 +89,8 @@ if __name__ == '__main__':
             password='123456',
         )
 
+
+def create_comments():
     print('create some comments')
     for article in Article.objects.all():
         user = User.objects.all().order_by('?').first()
@@ -99,4 +101,20 @@ if __name__ == '__main__':
                 content=fake.paragraph(),
             )
 
+
+if __name__ == '__main__':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chandeblog.settings")
+    django.setup()
+
+    from django.contrib.auth import get_user_model
+    from comment.models import Comment
+    from blog.models import Category, Article
+    User = get_user_model()
+
+    clean_database()
+    create_superuser()
+    create_users()
+    create_categories()
+    create_articles()
+    create_md_sample_article()
     print('done')
