111caf38344e14d4a639a4aea176b79d7c3f1c82
chandelures <chandelurewang@gmail.com>
2021-05-02 14:12:20 +0800

    test: add blog model tests

---
+++ blog/tests/test_models.py
@@ -0,0 +1,105 @@
+from io import BytesIO
+from PIL import Image as Ima
+from pathlib import Path
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.files.base import File
+
+from uuslug import slugify
+
+from blog.models import Article, Category, About, Image
+
+User = get_user_model()
+
+
+class ArticleModelTest(TestCase):
+    def setUp(self):
+        user = User.objects.create_superuser(
+            username='admin',
+            password='admin',
+        )
+        self.article = Article.objects.create(
+            title='测试标题',
+            abstract='测试内容',
+            content='测试内容',
+            author=user,
+        )
+
+    def test_str_representation(self):
+        self.assertEqual(self.article.__str__(), self.article.title)
+
+    def test_generate_slug(self):
+        self.assertEqual(self.article.slug, slugify(self.article.title))
+
+    def test_increase_views(self):
+        self.article.increase_views()
+        self.article.refresh_from_db()
+        self.assertEqual(self.article.views, 1)
+
+
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+            name="测试分类",
+        )
+
+    def test_str_representation(self):
+        self.assertEqual(self.category.__str__(), self.category.name)
+
+    def test_generate_slug(self):
+        self.assertEqual(self.category.slug, slugify(self.category.name))
+
+
+class AboutModelTest(TestCase):
+    def setUp(self):
+        user = User.objects.create_superuser(
+            username='admin',
+            password='admin',
+        )
+        article = Article.objects.create(
+            title='测试标题',
+            abstract='测试内容',
+            content='测试内容',
+            author=user,
+        )
+        self.about = About.objects.create(
+            article=article,
+        )
+
+    def test_str_representation(self):
+        self.assertEqual(self.about.__str__(), self.about.article.title)
+
+    def test_save(self):
+        other_about = About(
+            article=self.about.article,
+        )
+        other_about.save()
+        self.assertEqual(self.about, other_about)
+
+
+class ImageModelTest(TestCase):
+    def setUp(self):
+        self.image = Image.objects.create(
+            img=self.get_image_file()
+        )
+
+    @staticmethod
+    def get_image_file(name='test.png', ext='png', size=(50, 50), color=(256, 0, 0)):
+        file_obj = BytesIO()
+        image = Ima.new("RGBA", size=size, color=color)
+        image.save(file_obj, ext)
+        file_obj.seek(0)
+        return File(file_obj, name=name)
+
+    def test_str_representation(self):
+        self.assertEqual(self.image.__str__(), self.image.img.name)
+
+    def test_delete(self):
+        img_path = self.image.img.path
+        self.image.delete()
+        self.assertFalse(Path(img_path).exists())
+
+    def tearDown(self):
+        if self.image.pk:
+            self.image.delete()
