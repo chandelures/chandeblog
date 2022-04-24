979559b20a5ab7e941f4e8e6083b5d5232634699
chandelures <chandelurewang@gmail.com>
2022-04-24 20:19:54 +0800

    fix: use MethodView instead of Resource and use webargs instead of reqargs

--- app/utils/pagination.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import reqparse
-
-pagination_parser = reqparse.RequestParser()
-pagination_parser.add_argument("page", type=int, default=1, required=False)
-pagination_parser.add_argument("size", type=int, default=10, required=False)
-
-max_size = 50
