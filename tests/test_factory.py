26f309cdfb671627d66caaf9092d8da2b17b21b5
Chandelure Wang <chandelurewang@gmail.com>
2022-05-18 05:22:14 +0800

    test: optimize code (#61)

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -4,10 +4,10 @@ from app import create_app
 from app import wsgi
 
 
-def test_config():
+def test_config() -> None:
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
 
 
-def test_wsgi():
+def test_wsgi() -> None:
     assert isinstance(wsgi.app, Flask)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 from app.utils import slugify
 
 
-def test_slugify():
+def test_slugify() -> None:
     s = "test test test"
     assert " " not in slugify(s)
     s = "测试 测试 测试"
