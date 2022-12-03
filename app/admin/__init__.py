7566d32210513716c0f98591e40a61232a2db3be
chandelures <chandelurewang@gmail.com>
2022-12-03 23:05:53 +0800

    fix: update admin auth

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_admin import Admin, AdminIndexView, expose
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 
 from app.models.blog import Post
@@ -10,10 +10,16 @@ from app.utils.auth import base_auth
 
 class AuthAdminIndexView(AdminIndexView):
 
-    @base_auth.login_required
-    @expose('/')
-    def index(self):
-        return super().index()
+    def is_accessible(self):
+        auth = base_auth.get_auth()
+        password = base_auth.get_auth_password(auth)
+        user = base_auth.authenticate(auth, stored_password=password)
+        if user:
+            return True
+        return False
+
+    def inaccessible_callback(self, name, **kwargs):
+        return base_auth.auth_error_callback(status=401)
 
 
 class AuthModelView(ModelView):
@@ -26,6 +32,9 @@ class AuthModelView(ModelView):
             return True
         return False
 
+    def inaccessible_callback(self, name, **kwargs):
+        return base_auth.auth_error_callback(status=401)
+
 
 def init_app(app: Flask):
     admin = Admin(app,
@@ -33,4 +42,4 @@ def init_app(app: Flask):
                   name=app.config["SITENAME"],
                   template_mode="bootstrap4")
     admin.add_view(AuthModelView(Post, db.session))
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(AuthModelView(User, db.session))
